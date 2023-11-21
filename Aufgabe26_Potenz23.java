b3843aa9987baca39582f624e5fb6bb27f65febf
YayCover9000 <yassuanf@googlemail.com>
2023-11-21 18:52:17 +0100

    jetzt werden auch eingaben wie 3^-2 gefangen und somit eine ausgabe generiert die erklärt das die zahl nicht mit dezimalstellen representable ist

--- Aufgabe26_Potenz23.java
+++ Aufgabe26_Potenz23.java
@@ -1,85 +1,79 @@
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
 public class Aufgabe26_Potenz23 {
-	public static void main(String [] args) {
-					/*
-					• Gegeben sei ein sehr einfacher Taschenrechner, der nur die beiden
-					Grundrechenoperationen (+, -) beherrscht.
-					• Geben Sie für diesen Taschenrechner zwei Algorithmen zur Berechnung der Potenz an:
-					zahl1zahl2.
-					• Implementieren Sie den iterativen und rekursiven Algorithmus zum Potenzieren!
-					• Bemühen Sie sich um eine elegante Lösung. Verwenden Sie für diese Lösung
-					möglichst wenige Befehle. Auch das Abfragen von Sonderfällen bzw. Fehlern
-					sollten Sie auf ein Minimum reduzieren. (Sie werden sehen, dass die Mühe sich
-					lohnt. Die Struktur des Programms wird damit viel klarer. Damit reduziert sich auch
-					die Fehleranfälligkeit.)
-					• Versuchen Sie Ihren rekursiven Algorithmus mit Hilfe des Debuggers
-					nachzuvollziehen. (Sie werden sehen, dass Sie so die Rekursion besser verstehen
-					können.)
-					• Suchen Sie in der Java-API in der Klasse Math, ob es eine Methode zum Potenzieren
-					gibt. Falls ja, setzen Sie diese Methode ein, um zu überprüfen, ob Ihr Algorithmus
-					richtig rechnet.
-					*/
-		
-		//Mit addition multiplikation bauen -> dann methode multiplikation sooft aufrufen wie potenz steht
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Tippe Basis bestätige mit Enter dann tippe exponent bestätige mit Enter");
-		int basis = sc.nextInt();
-		int exponent = sc.nextInt();
-		
-//		if(zahl1<0||zahl2<0) {
-//			System.out.println("Eingabe darf nicht kleiner 0 sein"); //TODO aender weil negative exponenten auch zugelassen sind
-//		}else {
-		iterativ(basis,exponent);
-		BigDecimal result = rekursiv(basis, exponent);
-		System.out.println(result.stripTrailingZeros());
-        
-//		}
-	
-		
-		
-	}
-	private static void iterativ(int basis, int exponent) {
-		boolean exp = true;
-		int zwischen = 0;
-		double zwischenD = 0.0;
-		
-		if(exponent >= 0) {
-			if(exponent == 0) {
-				exp = false;
-			}else {
-				zwischen = basis;
-				for(int i = 1; i < exponent; i++) {
-					zwischen *= basis;
-				}
-			}
-				if (exp == false) {
-					System.out.println(1);
-				} else {
-				System.out.println(zwischen);
-				}
-		}
-			if (exponent < 0) {
-	            BigDecimal result = BigDecimal.ONE.divide(BigDecimal.valueOf(basis).pow(-exponent));
-	            System.out.println(result);
-	        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	}
-	private static BigDecimal rekursiv(int basis, int exponent) {
-		  if (exponent == 0) {
-	            return BigDecimal.ONE;
-	        } else if (exponent > 0) {
-	            BigDecimal temp = rekursiv(basis, exponent - 1);
-	            return BigDecimal.valueOf(basis).multiply(temp);
-	        } else {
-	            BigDecimal temp = rekursiv(basis, exponent + 1);
-	            BigDecimal reciprocal = BigDecimal.ONE.divide(BigDecimal.valueOf(basis), 10, RoundingMode.HALF_UP);
-	            return reciprocal.multiply(temp);
-	        }
-		
-	}
-	
+        try {
+            System.out.println("Tippe Basis, bestätige mit Enter, dann tippe Exponent, bestätige mit Enter:");
+            int basis = sc.nextInt();
+            int exponent = sc.nextInt();
+
+            iterativ(basis, exponent);
+
+            try {
+                BigDecimal result = rekursiv(basis, exponent);
+                System.out.println(result.stripTrailingZeros());
+            } catch (ArithmeticException e) {
+                System.out.println("Ungültige Eingabe: " + e.getMessage());
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Ungültige Eingabe: Bitte gib ganze Zahlen ein.");
+        } finally {
+            sc.close();
+        }
+    }
+
+    private static void iterativ(int basis, int exponent) {
+        boolean exp = true;
+        int zwischen = 0;
+
+        if (exponent >= 0) {
+            if (exponent == 0) {
+                exp = false;
+            } else {
+                zwischen = basis;
+                for (int i = 1; i < exponent; i++) {
+                    zwischen *= basis;
+                }
+            }
+            if (exp == false) {
+                System.out.println(1);
+            } else {
+                System.out.println(zwischen);
+            }
+        } else {
+            try {
+                BigDecimal result = BigDecimal.ONE.divide(BigDecimal.valueOf(basis).pow(-exponent));
+                System.out.println(result.stripTrailingZeros());
+            } catch (ArithmeticException e) {
+                System.out.println("Ungültige Eingabe: " + e.getMessage());
+            }
+        }
+    }
+
+    private static BigDecimal rekursiv(int basis, int exponent) {
+        if (exponent == 0) {
+            return BigDecimal.ONE;
+        } else if (exponent > 0) {
+            BigDecimal zwischen = rekursiv(basis, exponent - 1);
+            return BigDecimal.valueOf(basis).multiply(zwischen);
+        } else {
+            if (basis == 1) {
+                return BigDecimal.ONE; // Wenn Basis gleich 1 ist, direkt 1 zurückgeben
+            }
+
+            try {
+                BigDecimal zwischen = rekursiv(basis, exponent + 1);
+                BigDecimal einsUeberPotenz = BigDecimal.ONE.divide(BigDecimal.valueOf(basis), 20, RoundingMode.HALF_UP);
+                return einsUeberPotenz.multiply(zwischen);
+            } catch (ArithmeticException e) {
+                System.out.println("Ungültige Eingabe: " + e.getMessage());
+                return BigDecimal.ZERO;
+            }
+        }
+    }
 }
-
